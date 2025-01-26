c2babc4b3604bb9de5b186d4920026a4cfcb6eec
Udari Dissanayake <udaridissanayake8@gmail.com>
2025-01-26 22:37:44 +0530

    commit changes

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-import { Ripple, initMDB } from "mdb-ui-kit";
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = 
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100)/calcHeight);
+    if(pos > 220){
+        scrollProgress.style.display = "grid";
+    } 
+    else{
+        scrollProgress.style.display = "none";
+    }
 
-initMDB({ Ripple });
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(rgb(205, 0, 78) ${scrollValue}%,rgb(68, 2, 38) ${scrollValue}%)`;
+};
+
+window.onscroll= calcScrollValue;
+window.onload= calcScrollValue;
+
+// loader
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+
+    loader.classList.add("loader-hidden");
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild("loader");
+    });
+})
+
