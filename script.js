f2aa8ba97c144b2344621362f641301d6a8bae5e
Udari Dissanayake <udaridissanayake8@gmail.com>
2025-02-27 12:42:47 +0530

    ghj

--- script.js
+++ script.js
@@ -32,3 +32,11 @@ window.addEventListener("load", () => {
     });
 })
 
+var blocks =document.getElementsByClassName("block");
+var container = document.getElementsByClassName("container");
+var hs =new HorizontalScroll.default({
+    blocks: blocks,
+    container: container,
+    isAnimated: true,
+    springEfect: 0.8,
+});
